6aee170372bf25245d5acf3a5b17f4b2948f1e3f
korfvas@yahoo.gr <korfvas@yahoo.gr>
2019-08-10 11:21:48 +0300

    Add project files.

---
+++ Server4Hololens/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Server4Hololens
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
