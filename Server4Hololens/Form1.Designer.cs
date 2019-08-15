0d799f9b7cbae0064b3702e82daefc10cbb7953f
korfvas@yahoo.gr <korfvas@yahoo.gr>
2019-08-15 19:24:20 +0300

    Async Server

--- Server4Hololens/Form1.Designer.cs
+++ Server4Hololens/Form1.Designer.cs
@@ -28,13 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(58, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Server";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(139, 48);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(164, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(58, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Close Server";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(228, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Start Client";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(139, 29);
+            this.label1.Name = "lblIP";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 29);
+            this.label2.Name = "lblIPValue";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = " ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(368, 209);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Server4Hololens/Form1.cs
+++ Server4Hololens/Form1.cs
@@ -4,17 +4,213 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
+// Server = Listener
 namespace Server4Hololens
 {
     public partial class Form1 : Form
     {
+        private bool IsServerRunning = false;
+        private Socket handler;
+        private TcpListener tcpListener;
+        CancellationTokenSource tokenSource;
+        CancellationToken token;
+        public IPAddress serverAddress;
+        public int serverPort;
+
+        public delegate void OnSendResponseHandler(Socket handler);
+        public event OnSendResponseHandler OnSendResponse;
+        public void SendResponse(Socket handler)
+        {
+            Send(handler, textBox1.Text);
+        }
+
         public Form1()
         {
             InitializeComponent();
+            OnSendResponse += SendResponse;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!IsServerRunning)
+            {
+                StartServerAsync();
+                IsServerRunning = true;
+                tokenSource = new CancellationTokenSource();
+                token = tokenSource.Token;
+                var task = Task.Run(() =>
+                {
+                    while (!token.IsCancellationRequested)
+                    {
+                        ListenAsync();
+                    }
+                }, token);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (IsServerRunning)
+            {
+                CloseServer();
+                IsServerRunning = false;
+                Console.WriteLine("Closed Server");
+            }
+        }
+
+        //private IPEndPoint localEndPoint;
+        private Socket socketListener;
+        public static ManualResetEvent communicationCompleted = new ManualResetEvent(false);
+        // State object for reading client data asynchronously  
+        public class StateObject
+        {  
+            public Socket workSocket = null;
+            public const int BufferSize = 1024; 
+            public byte[] buffer = new byte[BufferSize]; 
+            public StringBuilder sb = new StringBuilder();
+        }
+
+        private void SetServerEndpointInfo()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[2];
+            serverAddress = ipAddress;
+            int port = 11000;
+            serverPort = port;
+            label2.Text = serverAddress.ToString() + " : " + serverPort.ToString();
+        }
+
+        private void StartServerAsync()
+        {
+            SetServerEndpointInfo();
+            IPEndPoint localEndPoint = new IPEndPoint(serverAddress, serverPort);
+            socketListener = new Socket(serverAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            socketListener.Bind(localEndPoint);
+            socketListener.Listen(1);
+            Console.WriteLine("Server has started");
+        }
+
+        private void ListenAsync()
+        {
+            communicationCompleted.Reset();
+            Console.WriteLine("Waiting for a connection...");
+            socketListener.BeginAccept(new AsyncCallback(AcceptConnectionHandler), socketListener);
+            communicationCompleted.WaitOne();
+        }
+
+        private void AcceptConnectionHandler(IAsyncResult ar)
+        {
+            communicationCompleted.Set();
+            Socket handler = socketListener.EndAccept(ar);
+  
+            StateObject state = new StateObject();
+            state.workSocket = handler;
+            handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadHandler), state);
+        }
+
+        private void ReadHandler(IAsyncResult ar)
+        {
+            String content = String.Empty;  
+            StateObject state = (StateObject)ar.AsyncState;
+            Socket handler = state.workSocket;
+ 
+            int bytesRead = handler.EndReceive(ar);
+            if (bytesRead < 1024)
+            {  
+                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
+                content = state.sb.ToString();
+                Console.WriteLine("Read {0} bytes from socket. \n Data : {1}", content.Length, content);
+                OnSendResponse(handler);
+            }
+            else
+            {
+                handler.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0, new AsyncCallback(ReadHandler), state);
+            }
+            
+        }
+
+        private void Send(Socket socket, String data)
+        { 
+            byte[] byteData = Encoding.ASCII.GetBytes(data);
+            socket.BeginSend(byteData, 0, byteData.Length, 0, new AsyncCallback(SendCallback), socket);
+        }
+
+        private void SendCallback(IAsyncResult ar)
+        {
+            try
+            { 
+                Socket handler = (Socket)ar.AsyncState; 
+                int bytesSent = handler.EndSend(ar);
+                Console.WriteLine("Sent {0} bytes to client.", bytesSent);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        private void CloseServer()
+        {
+            tokenSource.Cancel();
+            tcpListener.Stop();
+            label2.Text = "";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StartClient();
+        }
+
+        private void StartClient()
+        {
+            try
+            {
+                // Create a TcpClient.
+                // Note, for this client to work you need to have a TcpServer 
+                // connected to the same address as specified by the server, port
+                // combination.
+                TcpClient client = new TcpClient(serverAddress.ToString(), serverPort);
+
+                // Translate the passed message into ASCII and store it as a Byte array.
+                string message = "Send me something";
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);   
+                NetworkStream stream = client.GetStream();  // Get a client stream for reading and writing.
+                stream.Write(data, 0, data.Length); // Send the message to the connected TcpServer. 
+
+                Console.WriteLine("Sent: {0}", message);
+
+                // Receive the TcpServer.response.       
+                data = new Byte[256]; // Buffer to store the response bytes. 
+                String responseData = String.Empty;  // String to store the response ASCII representation.
+
+                // Read the first batch of the TcpServer response bytes.
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                Console.WriteLine("Received: {0}", responseData);
+
+                // Close everything.
+                stream.Close();
+                client.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e);
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e);
+            }
+
+            Console.WriteLine("\n Communication finished. Press Enter to continue...");
         }
     }
 }
